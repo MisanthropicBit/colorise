b568c79df9029be28cabfeaa3ed45ac3d3b2dcce
MisanthropicBit <albo.developer@gmail.com>
2019-05-23 16:31:44 +0200

    Update versions, pypy classifier

--- setup.py
+++ setup.py
@@ -38,12 +38,10 @@ setup(
         'Topic :: Terminals',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3'
-        'Programming Language :: Python :: 3.4'
-        'Programming Language :: Python :: 3.5'
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
-        'Programming Language :: Python :: PyPy'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ]
 )
